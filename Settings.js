979e18c451bcb04f45b1279cc1b7a96f678ae5d0
Thammarat Chokwattanakit <gen@abctech-thailand.com>
2016-08-08 17:21:52 +0700

    Update Settings.js

--- Settings.js
+++ Settings.js
@@ -1,6 +1,6 @@
 ﻿var organizationSettings = {
     //The url that points to team city
-    teamCityUrl: 'https://ci.travcorpservices.com',
+    teamCityUrl: 'https://teamcity.abct.io',
 
     //Only show builds for branches that satisfy the predicate
     branchFilter: function(branch) {
