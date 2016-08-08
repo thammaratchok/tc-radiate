ce0c16bc1b7b065507832108edcc2703935923c6
gen chokwattanakit <genchokwattanakit@gens-MacBook-Pro-3.local>
2016-08-08 17:29:37 +0700

    Fix setting

--- Settings.js
+++ Settings.js
@@ -1,6 +1,6 @@
 ﻿var organizationSettings = {
     //The url that points to team city
-    teamCityUrl: 'https://teamcity.abct.io',
+    teamCityUrl: 'http://teamcity.abct.io',
 
     //Only show builds for branches that satisfy the predicate
     branchFilter: function(branch) {
